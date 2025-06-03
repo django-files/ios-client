c578ba0a014f862515f3fc837ac637526dadf017
Ralph Luaces <16159770+raluaces@users.noreply.github.com>
2025-06-03 17:59:07 -0500

    just disable all ui tests for now

--- Django FilesUITests/Django_FilesUITests.swift
+++ Django FilesUITests/Django_FilesUITests.swift
@@ -7,21 +7,21 @@
 
 import XCTest
 
-final class Django_FilesUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+//final class Django_FilesUITests: XCTestCase {
+//
+//    override func setUpWithError() throws {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//
+//        // In UI tests it is usually best to stop immediately when a failure occurs.
+//        continueAfterFailure = false
+//
+//        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+//    }
+//
+//    override func tearDownWithError() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
+//
 //    @MainActor
 //    func testNewServer() throws {
 //        // UI tests must launch the application that they test.
@@ -37,7 +37,7 @@ final class Django_FilesUITests: XCTestCase {
 //
 //        // Use XCTAssert and related functions to verify your tests produce the correct results.
 //    }
-
+//
 //    @MainActor
 //    func testLaunchPerformance() throws {
 //        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
@@ -47,4 +47,4 @@ final class Django_FilesUITests: XCTestCase {
 //            }
 //        }
 //    }
-}
+//}
